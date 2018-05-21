3c788ec6bec8e3b6775755bedbca7cb1ff6b1f44
Vanderson Alves <vandersonalves15@gmail.com>
2018-05-21 18:19:59 -0300

    Send the user name in JWT access token

---
+++ src/main/java/com/example/algamoney/api/security/UsuarioSistema.java
@@ -0,0 +1,22 @@
+package com.example.algamoney.api.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import com.example.algamoney.api.model.Usuario;
+
+import lombok.Getter;
+
+@SuppressWarnings("serial")
+@Getter
+public class UsuarioSistema extends User{
+
+	private Usuario usuario;
+	
+	public UsuarioSistema(Usuario usuario, Collection<? extends GrantedAuthority> authorities) {
+		super(usuario.getEmail(), usuario.getSenha(), authorities);
+		this.usuario = usuario;
+	}
+}
